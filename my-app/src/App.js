681c82631a8d06b816a7d1efaee8f375a586d43a
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-10-08 09:55:45 +0300

    Add FunctionalGreetingWithComponent

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './Components/FunctionalGreeting';
+import FunctionalGreetingwithProps from './Components/FunctionalGreetingWithProps';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />    </div>
+      <FunctionalGreeting />
+      <FunctionalGreetingwithProps />
+    </div>
   );
 }
 
---
+++ my-app/src/Components/FunctionalGreetingWithProps.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const FunctionalGreetingwithProps = () => <h1>Hey</h1>
+
+export default FunctionalGreetingwithProps;
